bd1633b6cd7caa09ff7a288d48931c95700a4480
sanskar021 <sanskarchaurey2000@gmail.com>
2023-05-30 20:34:57 +0530

    first commit

---
+++ models/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', true);
+
+mongoose
+    .connect("mongodb://127.0.0.1:27017/task")
+    .then((result) => {
+        console.log("database connected!");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
